e66f3b4ddda6a6b9136a7b977d42f00e0a2e7dd1
JacobFJ <min.mingi1997@gmail.com>
2022-11-11 15:19:37 +0800

    feat: init

---
+++ App.tsx
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * Install - ext install EditorConfig
+ * @format
+ * @flow strict-local
+ */
+import React from "react";
+
+const App: React.FC = () => {
+  return <></>;
+};
+
+export default App;
