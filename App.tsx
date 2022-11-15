5d633722e2056bf0d9f1dc673ce9135017afce3d
JacobFJ <min.mingi1997@gmail.com>
2022-11-15 15:02:12 +0800

    test

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import React from "react";
 import { Text } from "react-native";
 
 const App = () => {
-  return <Text>Hello, World</Text>;
+  return <Text>{Math.random(2)}</Text>;
 };
 
 export default App;
